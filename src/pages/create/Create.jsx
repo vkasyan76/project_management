f4403e62715dd44f2502e3437bb9f1bea3de3abf
vkasyan76 <vkasyan@gmx.de>
2022-10-06 00:25:13 +0200

    Set Form Errors

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -24,6 +24,7 @@ export default function Create() {
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
 
   useEffect(() => {
     if (documents) {
@@ -36,6 +37,16 @@ export default function Create() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setFormError(null)
+    if (!category) {
+      setFormError('Please select a project category')
+      return
+    }
+    if (assignedUsers < 1) {
+      setFormError('Please assign the project to least one user')
+      return
+    }
+
     console.log(name, details, dueDate, category.value, assignedUsers)
   }
 
@@ -90,6 +101,8 @@ export default function Create() {
         </label>
 
         <button className="btn">Add Project</button>
+
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   )
