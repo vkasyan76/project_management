a9b2a4cb1307e9e98110b34de2205ba728444bf1
vkasyan76 <vkasyan@gmx.de>
2022-10-22 22:04:45 +0200

    Making a filter (Part 1)

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useCollection } from '../../hooks/useCollection'
 import ProjectList from '../../components/ProjectList'
+import ProjectFilter from './ProjectFilter'
 
 import './Dashboard.css'
 
@@ -10,7 +11,7 @@ function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
-
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
