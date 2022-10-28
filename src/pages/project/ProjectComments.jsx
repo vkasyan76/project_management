ba80f60ccf840f384010e973422ba37d83d33742
vkasyan76 <vkasyan@gmx.de>
2022-10-28 14:45:44 +0200

    Time elapced in ProjectComments

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -3,6 +3,7 @@ import { timestamp } from '../../firebase/config'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { v4 as uuidv4 } from 'uuid'
 import { useFirestore } from '../../hooks/useFirestore'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 import Avatar from '../../components/Avatar'
 
 export default function ProjectComments({ project }) {
@@ -42,7 +43,11 @@ export default function ProjectComments({ project }) {
                 <p>{comment.displayName}</p>
               </div>
               <div className="comment-date">
-                <p>date here</p>
+                <p>
+                  {formatDistanceToNow(comment.createdAt.toDate(), {
+                    addSuffix: true,
+                  })}
+                </p>
               </div>
               <div className="comment-content">
                 <p>{comment.content}</p>
